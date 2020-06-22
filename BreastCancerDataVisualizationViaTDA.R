e29d60e86a4595046eab779bacdadb566eb76c22
snigdhayan <snigdhayan@gmail.com>
2020-06-22 23:01:06 +0200

    Initial commit

---
+++ BreastCancerDataVisualizationViaTDA.R
@@ -0,0 +1,28 @@
+library(TDA)
+
+# read data
+setwd('/Users/ibatu/Documents/MyProgramsWindows/R/TopologicalDataAnalysis/')
+data <- read.csv(file = 'breast_cancer_dataset.csv', header = TRUE)
+data <- data[names(data)!='label']
+
+# configuration parameters
+sample_size = 50
+n_samples = 5
+wasserstein_exp = 1
+wasserstein_dim = 0
+
+# draw 2 samples and compute persistent homologies
+my_list <- list()
+for (i in 1:n_samples) {
+  data_sample <- data[sample(nrow(data), sample_size , replace = FALSE), ]
+  my_list[[i]] <- calculate_homology(data_sample)
+}
+
+# compute wasserstein distance between samples
+distance_matrix <- matrix(nrow = n_samples, ncol = n_samples)
+for (row in 1:nrow(distance_matrix)) {
+  for (col in 1:ncol(distance_matrix)) {
+    distance_matrix[row,col]<- wasserstein(my_list[[row]],my_list[[col]], p = wasserstein_exp, dimension = wasserstein_dim)
+  }
+}
+distance_matrix
