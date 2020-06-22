520007f0f8cd903589bf6549aac996d34f7d753a
snigdhayan <snigdhayan@gmail.com>
2020-06-22 23:24:00 +0200

    Improved inline comments

--- BreastCancerDataAnalysisViaTDA.R
+++ BreastCancerDataAnalysisViaTDA.R
@@ -11,7 +11,7 @@ n_samples = 5
 wasserstein_exp = 1
 wasserstein_dim = 0
 
-# draw 2 samples and compute persistent homologies
+# draw samples and compute persistent homologies
 my_list <- list()
 for (i in 1:n_samples) {
   data_sample <- data[sample(nrow(data), sample_size , replace = FALSE), ]
@@ -25,4 +25,6 @@ for (row in 1:nrow(distance_matrix)) {
     distance_matrix[row,col]<- wasserstein(my_list[[row]],my_list[[col]], p = wasserstein_exp, dimension = wasserstein_dim)
   }
 }
+
+# print wasserstein distances as a matrix
 distance_matrix
